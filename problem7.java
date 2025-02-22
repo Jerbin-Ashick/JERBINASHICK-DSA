ced72701d819dcad42addd26595fc3d8ff4d6a1d
Jerbin_Ashick <jerbin2626@gmail.com>
2025-02-22 20:01:45 +0530

    Add files via upload

---
+++ problem7.java
@@ -0,0 +1,16 @@
+package week1;
+import java.util.*;
+public class problem7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String firstName=sc.nextLine();
+		String lastName=sc.nextLine();
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			System.out.println(firstName+lastName);
+		}
+	}
+
+}
