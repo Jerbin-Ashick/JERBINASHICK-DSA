a55f772cd1c1daa16aed07a70432f41da5369a55
Jerbin_Ashick <jerbin2626@gmail.com>
2025-02-22 19:49:12 +0530

    Add files via upload

---
+++ problem5.java
@@ -0,0 +1,14 @@
+package week1;
+import java.util.*;
+public class problem5 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		for(int i=0;i<y;i++) {
+			System.out.println(x);
+		}
+	}
+
+}
