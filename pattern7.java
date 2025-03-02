ff831189d9019b6604430ad86d087edf9d85c289
Jerbin_Ashick <jerbin2626@gmail.com>
2025-03-02 10:49:03 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,19 @@
+package week2;
+
+import java.util.Scanner;
+
+public class pattern7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=n;i>=1;i--) {
+			for(int j=i;j>=1;j--) {
+				System.out.print(j);
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
